06ea8e9f8a4478bdc354ea2bb9802459007536ea
czp970118 <493748703@qq.com>
2026-03-25 22:12:20 +0800

    feat:图片上传阿里

--- react-vite/src/main.tsx
+++ react-vite/src/main.tsx
@@ -1,22 +1,21 @@
-import ReactDOM from 'react-dom/client'
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import { ConfigProvider } from 'antd';
-import App from './App.tsx'
-import Login from './pages/login';
-import Regist from './pages/regist';
-import zhCN from 'antd/lib/locale/zh_CN';
-import 'antd/dist/reset.css';
-import './index.css'
+import ReactDOM from "react-dom/client";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { ConfigProvider } from "antd";
+import App from "./App.tsx";
+import Login from "./pages/login";
+import Regist from "./pages/regist";
+import zhCN from "antd/lib/locale/zh_CN";
+import "antd/dist/reset.css";
+import "./index.css";
 
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <Router>
-    <ConfigProvider locale={zhCN} >
-      <Routes>
-        <Route path='/' element={<Login />} />
-        <Route path='/regist' element={<Regist />} />
-        <Route path='/pages/*' element={<App />} />
-      </Routes>
-    </ConfigProvider>
-  </Router>
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+   <Router>
+      <ConfigProvider locale={zhCN}>
+         <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/regist" element={<Regist />} />
+            <Route path="/pages/*" element={<App />} />
+         </Routes>
+      </ConfigProvider>
+   </Router>
+);
